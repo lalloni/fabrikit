612fbe3990496fece95efeaffa043e42259df94d
Pablo Lalloni <plalloni@gmail.com>
2019-08-01 10:58:54 -0300

    Add commonly used functions for declaring Store Composite's Schema

---
+++ chaincode/storeutil/schema.go
@@ -0,0 +1,20 @@
+package storeutil
+
+import (
+	"strconv"
+
+	"github.com/lalloni/fabrikit/chaincode/store"
+	"github.com/lalloni/fabrikit/chaincode/store/key"
+)
+
+func Uint64Key(name string) store.KeyFunc {
+	return func(id interface{}) (*key.Key, error) {
+		return key.NewBase(name, strconv.FormatUint(id.(uint64), 10)), nil
+	}
+}
+
+func Uint64Identifier(seg int) store.ValFunc {
+	return func(k *key.Key) (interface{}, error) {
+		return strconv.ParseUint(k.Base[seg].Value, 10, 64)
+	}
+}
